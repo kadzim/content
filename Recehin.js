8af92501149dd5623562b708067a3e0428601efc
kadzim <adzim154@gmail.com>
2016-12-21 04:45:58 +0700

    Add files via upload

---
+++ Recehin.js
@@ -0,0 +1,17 @@
+<script type='text/javascript'><br />
+// ==========================================================================================<br />
+// script safelink for blogspot by Lutfi Setiyawan<br />
+// powered by Artikelious.com and Architecture.builders<br />
+// private publish at 24/05/2015<br />
+// All rights reserved<br />
+// ==========================================================================================<br />
+// jangan pernah menghapus apapun di dalam script ini kecuali yang disarankan!<br />
+// jika ingin script ini work di blog Anda!<br />
+// ================================ ganti bagian ini ========================================<br />
+var blog_kamu = 'www.godollarin.blogspot.com'; // tanpa "http://", contoh : www.artikelious.com<br />
+// =================================== trade mark ===========================================<br />
+// jangan mengganti bagian ini ataupun yang lain, kecuali hanya bagian "blog_kamu" diatas!!!<br />
+var izin_dulu_ya = 'yang_maling_cacat_permanen';<br />
+</script><br />
+<script src='http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/aes.js'/><br />
+<script src='http://www.architecture.builders/artikelious/jquery1-7-1min.js'/><br />
