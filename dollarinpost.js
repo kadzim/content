8ae7e7af72fab1b27c3ecb7b2f8a525b55d3ec00
kadzim <adzim154@gmail.com>
2016-12-21 10:40:50 +0700

    Update dollarinpost.js

--- dollarinpost.js
+++ dollarinpost.js
@@ -20,7 +20,7 @@
     {a_to_vh=true;}
     j++;}
     if(a_to_vh==false)
-    {var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://godollarin.blogspot.sg/2016/12/go-dollarin.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+    {var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://godollarin.blogspot.sg/2016/12/sejarah-dan-perkembangan-operating.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
     var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
     {a_to_vj.innerHTML+=a_to_vb;}
     if(a_to_vk)
